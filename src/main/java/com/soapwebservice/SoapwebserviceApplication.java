8e812299dcd6a71f8fe4a18ec9f600c6ab081e4f
Guzman <francisco.guzman@accenture.com>
2024-09-09 15:09:10 -0400

    lab done

---
+++ src/main/java/com/soapwebservice/SoapwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.soapwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwebserviceApplication.class, args);
+	}
+
+}
